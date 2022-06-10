9d6f0cd4703ba7eacbb76df2f3b9ea624251de52
LucasHeliab <lucas.heliab.dev@gmail.com>
2022-06-10 15:29:31 -0300

    Estilização footer

--- front/components/footer.php
+++ front/components/footer.php
@@ -1,12 +1,26 @@
 <footer>
-  <ul>
     <ul>
-      <li id="servicos" class="col-3">Serviços</li>
+      <li id="servicos" class="col-3">
+        <h3>Serviços</h3>
+        <ul>
+          <?php
+            include('conexao.php');
+
+            $sql_code = "SELECT nome FROM servicos";
+            $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL:" . $mysqli->error);
+
+            while ($servico = $sql_query->fetch_row()){
+              $text = '<li id="item" class="col-3">'.$servico[0].'</li>';
+              echo $text;
+            }
+          ?>
+        </ul>
+      </li>
     </ul>
     <ul>
-      <li id="medicos" class="col-3">Médicos</li>
+      <li id="medicos" class="col-3"><h3>Médicos</h3></li>
     </ul>
     <ul>
-      <li id="contato" class="col-3">Contato</li>
+      <li id="contato" class="col-3"><h3>Contato</h3></li>
     </ul>
 </footer>
